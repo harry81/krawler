7a315b012c120fed6a97be59e7044f1eab89349c
Hyunmin <pointer81@gmail.com>
2019-07-09 15:27:05 +0900

    Add hani

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from water import naver
+from water import naver, hani
 
 
 class TestStringMethods(unittest.TestCase):
@@ -10,6 +10,24 @@ class TestStringMethods(unittest.TestCase):
     def test_cafe_10050146(self):
         res = naver.cafe(clubid='10050146')
         print(res)
+
+
+class TestHani(unittest.TestCase):
+
+    def test_get_hrefs(self):
+        hrefs = hani.get_hrefs()
+        self.assertIsInstance(hrefs[0], str)
+
+    def test_extract(self):
+        hrefs = hani.get_hrefs()
+
+        print(len(hrefs))
+
+        for href in hrefs[:40]:
+            article = hani.extract(href)
+            print(article['title'], article['register'], article['url'])
+
+        self.assertIsInstance(article['title'], str)
 
 
 if __name__ == '__main__':
